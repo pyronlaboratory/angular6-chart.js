d9f68a9bcf28bdd3acbd323251b8d23c4f65571e
Ronit Mishra <ronnie94official@gmail.com>
2018-09-29 17:40:30 +0530

    Update app.component.ts

--- charts/src/app/app.component.ts
+++ charts/src/app/app.component.ts
@@ -4,9 +4,11 @@ import { Chart } from 'chart.js';
 
 
 @Component({
+  
  selector: 'app-root',
-	    templateUrl: './app.component.html',
-	    styleUrls: ['./app.component.css']
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
+  
 })
 
 
